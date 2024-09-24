4948cf78ff26ca9017efce83124c8950e5e82372
Mahadev Chidanand Athani <athanimahadev348@gmail.com>
2024-09-24 19:05:13 +0530

    create new gui using tkinter

--- Day27/gui.py
+++ Day27/gui.py
@@ -1,1 +1,12 @@
-hgjhjgjkhjkjgk
+import tkinter
+
+window = tkinter.Tk()
+window.title("My first Gui")
+window.minsize(500, 300)
+
+# label
+my_label = tkinter.Label(text="I am Mahadev", font=("Arial", 24, "bold"))
+# To display the text at center
+my_label.pack()
+
+window.mainloop()
